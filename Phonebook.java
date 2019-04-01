3c45b0294cacbe815c643285c17ff2a41940e13d
lapenkovdaniil <daniil.adst@gmail.com>
2019-04-01 12:57:39 +0300

    Version 4

--- Phonebook.java
+++ Phonebook.java
@@ -3,13 +3,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Phonebook {
+class Phonebook {
     private  Map<String, List<String>> pb = new HashMap<>();
-    public List<String> getNumber(String name){
+    List<String> getNumber(String name){
         if (!this.pb.containsKey(name)) throw new IllegalArgumentException("Контакт" + name + " не существует");
         return this.pb.get(name);
     }
-    public List<String> getContact(String phone) {
+    List<String> getContact(String phone) {
         List<String> contactList = new ArrayList<String>() {};
         for (Map.Entry<String, List<String>> entry : this.pb.entrySet()) {
             String key = entry.getKey();
@@ -18,18 +18,18 @@ public class Phonebook {
         }
         return contactList;
     }
-    public Map<String, List<String>> addContact(String name){
+    Map<String, List<String>> addContact(String name){
         if(this.pb.containsKey(name))throw new IllegalArgumentException("Контакт" + name + " уже существует");
         List<String> phone = new ArrayList<String>();
         this.pb.put(name,phone);
         return this.pb;
     }
-    public Map<String, List<String>> delContact(String name) {
+    Map<String, List<String>> delContact(String name) {
         if (!this.pb.containsKey(name)) throw new IllegalArgumentException("Контакт" + name + "не найден");
         this.pb.remove(name);
         return this.pb;
     }
-    public Map<String, List<String>> addNumber(String name,String phone){
+    Map<String, List<String>> addNumber(String name, String phone){
         if(!this.pb.containsKey(name)) throw new IllegalArgumentException("Контакт" + name + " не существует");
         if (!phone.matches("^[0-9+*#/-]+")) throw new IllegalArgumentException("Неверный формат номера");
         List<String> contactList = this.pb.get(name);
@@ -37,7 +37,7 @@ public class Phonebook {
         this.pb.get(name).add(phone);
         return this.pb;
     }
-    public Map<String, List<String>> delNumber(String name,String phone){
+    Map<String, List<String>> delNumber(String name, String phone){
         if(!this.pb.containsKey(name)) throw new IllegalArgumentException("Контакт" + name + " не существует");
         List<String> contactList = this.pb.get(name);
         if (!contactList.contains(phone)) throw new IllegalArgumentException("Номер " + phone + " не существует");
