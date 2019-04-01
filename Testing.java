772fa036f4bba695773adff7a6292e015c0a1222
lapenkovdaniil <daniil.adst@gmail.com>
2019-04-01 12:58:24 +0300

    Version 4.0

--- Testing.java
+++ Testing.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class Testing {
-    // Проверять по очереди
     Map<String, List<String>> map = new HashMap<>();
     Phonebook pb = new Phonebook();
     List<String> numbersOfPhone = new ArrayList<>();
